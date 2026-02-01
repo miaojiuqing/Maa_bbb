b72b1a4461ae049e9d6cd23df5024ce36d4fa3ec
miaojiuqing <ll13400709119@163.com>
2026-02-01 15:28:01 +0800

    fix: 应该是修复了大量选项不生效的bug，感谢插件提醒(不过还剩一个不知道啥情况

--- AAAAAAAA.py
+++ AAAAAAAA.py
@@ -1,7 +1,11 @@
-a = 13
-b = 34
-a = a + b
-b = a - b
-a = a - b
-print(a, b)
-range(10)
+# a = 13
+# b = 34
+# a = a + b
+# b = a - b
+# a = a - b
+# print(a, b)
+# range(10)
+
+# a = 2
+# a**= 3//2的三次方
+# print(a)
