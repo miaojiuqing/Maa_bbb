e8f047a8c5cac811f12c98b17a83b9de45f6d0d9
miaojiuqing <ll13400709119@163.com>
2026-06-01 03:00:59 +0800

    ci: 尝试修复部署不检查的bug

--- agent/main.py
+++ agent/main.py
@@ -32,15 +32,15 @@ def main():
 
 if __name__ == "__main__":
     # 在运行主程序之前进行部署检查
-    # git_path = get_main_py_path().parent.parent / ".git"
-    # if git_path.exists():
-    #    print("测试模式,. 不进行部署检查")
-    #    if len(sys.argv) == 1:
-    #        sys.argv.append("MAA_AGENT_SOCKET")
-    # elif not deploy():
-    #    print("error: 部署检查失败，程序退出")
-    #    sys.exit(1)
-    # from agent.CustomFile import *
+    git_path = get_main_py_path().parent.parent / ".git"
+    if git_path.exists():
+        print("测试模式，不进行部署检查")
+        if len(sys.argv) == 1:
+            sys.argv.append("MAA_AGENT_SOCKET")
+    elif not deploy():
+        print("error: 部署检查失败，程序退出")
+        sys.exit(1)
+    from agent.CustomFile import *
 
     try:
         main()
