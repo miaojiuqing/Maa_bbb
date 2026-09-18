cb2b1f6334cf63db1b9ceb00e29e9cc5231d2110
overflow65537 <err.overflow@gmail.com>
2026-09-18 19:23:58 +0800

    refactor: 注释掉部署检查相关代码

--- agent/main.py
+++ agent/main.py
@@ -12,7 +12,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from agent.deploy.deploy import deploy, get_main_py_path
+#from agent.deploy.deploy import deploy, get_main_py_path
 
 
 def main():
@@ -32,14 +32,14 @@ def main():
 
 if __name__ == "__main__":
     # 在运行主程序之前进行部署检查
-    git_path = get_main_py_path().parent.parent / ".git"
+    """git_path = get_main_py_path().parent.parent / ".git"
     if git_path.exists():
         print("测试模式，不进行部署检查")
         if len(sys.argv) == 1:
             sys.argv.append("MAA_AGENT_SOCKET")
     elif not deploy():
         print("error: 部署检查失败，程序退出")
-        sys.exit(1)
+        sys.exit(1)"""
     from agent.CustomFile import *
 
     try:
